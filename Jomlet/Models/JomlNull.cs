24a6ef9bfff521e98500a6176710aa95bd768775
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-08 15:52:14 +0100

    fix: `JomlNull.Value` should be the deserialized one

--- Jomlet/Models/JomlNull.cs
+++ Jomlet/Models/JomlNull.cs
@@ -3,8 +3,8 @@
 public class JomlNull : JomlValue
 {
     public static JomlNull Instance = new();
-    private const string _Value = "null";
-    public string Value => _Value;
-    public override string StringValue => Value;
+    private const string NullString = "null";
+    public object? Value => null;
+    public override string StringValue => NullString;
     public override string SerializedValue => StringValue;
 }
